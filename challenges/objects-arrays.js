8e2535a2664f4c3ce9d1cc4a40f32976ae69591b
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-04-08 23:19:30 -0500

    DONE

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -90,18 +90,18 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-/*
+
 let uni = [];
 for(let i=0; i < graduates.length; i++){
     
-  if (graduates[i].university.contains('Uni')) {
+  if (graduates[i].university.includes('Uni')) {
   uni.push(graduates[i].university);}
   
   
 }
 
 console.log(uni);
-*/
+
 
 
 // ==== ADVANCED Array Methods ====
